92941d89c338f0a81599af37ef24806726de6e81
Jasper935 <pikunivan@gmail.com>
2022-06-29 00:07:27 +0300

    upd

---
+++ src/index.js
@@ -0,0 +1,69 @@
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
+
+const DEBOUNCE_DELAY = 300;
+
+const searchField = document.querySelector('#search-box');
+
+searchField.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(event) {
+  const value = event.target.value.trim();
+  list.innerHTML = '';
+  div.innerHTML = '';
+
+  if (value === '') {
+    return;
+  }
+
+  fetchCountries(value).then(response => {
+    createMarkUp(response);
+  });
+}
+import Notiflix from 'notiflix';
+
+const list = document.querySelector('.country-list');
+const div = document.querySelector('.country-info');
+
+function createMarkUp(arr) {
+  list.innerHTML = '';
+  div.innerHTML = '';
+
+  if (arr.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+    return;
+  } else if (arr.length >= 2) {
+    listOfCountries(arr);
+  } else {
+    showCountry(arr);
+  }
+}
+
+function listOfCountries(arr) {
+  const countryList = arr
+    .map(element => {
+      return `<li class ="li-list"><img width='25' class ="list-flag" src="${element.flags.svg}" alt="${element.name}"/> <h2>${element.name}</h2></li>`;
+    })
+    .join('');
+
+  list.innerHTML = countryList;
+}
+
+function showCountry(arr) {
+  const markUp = `<h2 class ="h2-div">${arr[0].name}</h2><img src ="${
+    arr[0].flags.svg
+  }" alt="${arr[0].name}" width ="200"/><p>Capital: <span>${
+    arr[0].capital
+  }</span></p><p>Population: <span>${arr[0].population}</span></p>
+  <p>Languages: <span>${arr[0].languages
+    .map(elem => elem.name)
+    .join(', ')}</span></p>`;
+
+  div.innerHTML = markUp;
+}
+
+export { createMarkUp, list, div };
+
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,17 @@
+import Notiflix from 'notiflix';
+
+export function fetchCountries(name) {
+  const baseUrl = 'https://restcountries.com/v2/name/';
+  const response = fetch(`${baseUrl}${name}`)
+    .then(data => {
+      if (!data.ok) {
+        console.log(data);
+        throw new Error('error');
+      }
+      return data.json();
+    })
+    .catch(() =>
+      Notiflix.Notify.failure('Oops, there is no country with that name!')
+    );
+  return response;
+}
